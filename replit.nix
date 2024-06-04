d25b8a77d1db42a1eaea59b41b075d8e7f8ec5c5
0kbrooks <okbrooks@cps.edu>
2024-06-04 16:05:12 +0000

    fuck it we ball: pick and place html element :)

--- replit.nix
+++ replit.nix
@@ -1,5 +1,7 @@
 {pkgs}: {
   deps = [
+    pkgs.vim-full
+    pkgs.vim
     pkgs.zip
     pkgs.openssh
   ];
--- src/App.jsx
+++ src/App.jsx
@@ -3,53 +3,65 @@ import * as React from "react";
 import { useState } from "react";
 let mousePos = { x: 0, y: 0 };
 function DropBox() {
-  const [mouseOver, setMouseOver] = useState(false)
-  return <div className={mouseOver ? "dropBox mouseOver" : "dropBox"} onMouseEnter={() => setMouseOver(true)} onMouseLeave={() => setMouseOver(false)}>grough</div>;
+  const [mouseOver, setMouseOver] = useState(false);
+  return (
+    <div
+      className={mouseOver ? "dropBox mouseOver" : "dropBox"}
+      onMouseEnter={() => setMouseOver(true)}
+      onMouseLeave={() => setMouseOver(false)}
+    >
+      grough
+    </div>
+  );
 }
 function DropItem() {
   const [dragging, setDragging] = useState(false);
   const [draggingIntervalId, setDraggingIntervalId] = useState();
-  const [style,setStyle] = useState({
-    position:"fixed",
-    top:"100px",
-    left:"100px"
-  })
+  const [style, setStyle] = useState({
+    position: "fixed",
+  });
   function draggingPeriodic() {
-    let newStyle = {}
-    Object.assign(newStyle,style)
-    newStyle.left = mousePos.x-10+"px"
-    newStyle.top = mousePos.y-10+"px"
-    setStyle(newStyle)
+    let newStyle = {};
+    Object.assign(newStyle, style);
+    newStyle.left = mousePos.x - 10 + "px";
+    newStyle.top = mousePos.y - 10 + "px";
+    setStyle(newStyle);
   }
-  function mouseDownHandler() {
+  function mouseDownHandler(event) {
     setDragging(true);
     if (!draggingIntervalId) {
       setDraggingIntervalId(setInterval(draggingPeriodic, 100));
     }
+    event.target.style.position="absolute"
   }
-  function mouseUpHandler() {
+  function mouseUpHandler(event) {
     setDragging(false);
     clearInterval(draggingIntervalId);
     setDraggingIntervalId(null);
-    for(let i of document.getElementsByClassName("dropBox")){
-      console.log(i)
-    }
+    let oldElement = document.elementFromPoint(mousePos.x,mousePos.y)
+    oldElement.remove()
+    document.elementFromPoint(mousePos.x,mousePos.y).appendChild(oldElement)
+    event.target.style.position="static"
+    console.log(event.target.style.position)
   }
   return (
     <span
       style={style}
       onMouseDown={mouseDownHandler}
       onMouseUp={mouseUpHandler}
+      className="penis"
     >
       pegnis
     </span>
   );
 }
 async function afterLoad() {
-  await new Promise((resolve) => window.addEventListener("load", resolve))
-  document.addEventListener("pointermove", (event) => {mousePos = {x:event.clientX,y:event.clientY};})
+  await new Promise((resolve) => window.addEventListener("load", resolve));
+  document.addEventListener("pointermove", (event) => {
+    mousePos = { x: event.clientX, y: event.clientY };
+  });
 }
-afterLoad()
+afterLoad();
 export default function App() {
   return (
     <div id="parend">
